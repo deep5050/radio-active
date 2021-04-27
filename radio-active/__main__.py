03da20c53d77a31264d54c7869cf69d9babe27dc
Dipankar Pal <dipankarpal5050@gmail.com>
2021-04-27 15:04:36 +0530

    fix: minor bugs v1.1.0

---
+++ radio-active/__main__.py
@@ -0,0 +1,115 @@
+#!/usr/bin/ python
+
+import sys
+import signal
+import argparse
+import json
+from zenlog import log
+from handler import Handler
+from player import Player
+
+player = None
+
+
+def signal_handler(sig, frame):
+    global player
+    log.debug("You pressed Ctrl+C!")
+    log.debug("Stopping the radio")
+    if player.is_playing:
+        player.stop()
+    log.info("Exiting now")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Play any radio around the globe right from the CLI ",
+        prog="radio-active",
+    )
+
+    parser.add_argument("--version", action="version", version="%(prog)s 1.0")
+
+    parser.add_argument(
+        "--station",
+        action="store",
+        dest="station_name",
+        help="Specify a station name",
+    )
+    parser.add_argument(
+        "--uuid",
+        action="store",
+        dest="station_uuid",
+        help="Specify a station UUID",
+    )
+
+    parser.add_argument(
+        "--log-level",
+        action="store",
+        default="info",
+        dest="log_level",
+        help="Specify log level",
+    )
+
+    args = parser.parse_args()
+
+    station_name = args.station_name
+    station_uuid = args.station_uuid
+    log_level = args.log_level
+
+    if log_level in ["info", "error", "debug"]:
+        log.level(log_level)
+    else:
+        log.level("info")
+        log.warning("Correct log levels are: error,warning,info(default),debug")
+
+    if station_name is None and station_uuid is None:
+        # try to fetch the last played station's information
+        log.warn(
+            "No station information provided, trying to get the last station information"
+        )
+        try:
+            with open("last_station.json", "r") as f:
+                last_station = json.load(f)
+                f.close()
+                station_uuid = last_station["stationuuid"]
+        except Exception as e:
+            log.critical("Need a station name  or UUID to play the radio, see help")
+            sys.exit(0)
+
+    # log.debug("Station name: " + station_name)
+
+    handler = Handler()
+    target_station = None
+    # uuid gets first preference
+
+    if station_uuid is not None and station_name is not None:
+        log.info(
+            "Both station name and UUID are provided, selecting UUID to find the station"
+        )
+
+    if station_uuid is not None:
+        log.debug("Started with UUID mode")
+        handler.play_by_station_uuid(station_uuid)
+
+    if station_uuid is None and station_name is not None:
+        handler.play_by_station_name(station_name)
+
+    global player
+    player = Player(handler.target_station["url"])
+
+    # writing the station name to a file, next time if user
+    # don't specify anything will try to start the last station
+
+    log.debug("Dumping station information")
+    with open("last_station.json", "w") as f:
+        json.dump(handler.target_station, f)
+        f.close()
+
+    signal.pause()
+
+
+if __name__ == "__main__":
+    main()
---
+++ radio-active/handler.py
@@ -0,0 +1,62 @@
+import sys
+import signal
+from zenlog import log
+from player import Player
+from pyradios import RadioBrowser
+
+
+class Handler:
+    def __init__(self):
+        self.API = None
+        self.response = None
+        self.target_station = None
+
+        self.API = RadioBrowser()
+
+    def station_validator(self):
+        if len(self.response) == 0:
+            log.error("No stations found by the name")
+            sys.exit(0)
+        if len(self.response) > 1:
+            log.info("Multiple stations found by the name")
+            stations_name = ""
+            for station in self.response:
+                # stations_name = stations_name + "," + station["name"]
+                log.info(
+                    "name: {} | uuid: {} | country: {}".format(
+                        station["name"], station["stationuuid"], station["country"]
+                    )
+                )
+
+            # log.info(stations_name)
+            sys.exit(0)
+        if len(self.response) == 1:
+            log.info("Station found: {}".format(self.response[0]["name"]))
+            log.debug(self.response[0])
+            self.target_station = self.response[0]
+            self.API.click_counter(self.target_station["stationuuid"])
+
+    def play_by_station_name(self, _name=None):
+        # print(_name)
+        self.response = self.API.search(name=_name, name_exact=False)
+        self.station_validator()
+
+    def play_by_station_uuid(self, _uuid):
+        # print(_uuid)
+        # Pyradios by default don't let you search by uuid
+        # a trick is to call click_counter(uuid) directly to get the statioon info
+        is_ok = "false"
+        try:
+            self.target_station = self.API.click_counter(_uuid)
+            log.debug(self.target_station)
+            is_ok = self.target_station["ok"]
+        except Exception as e:
+            log.error("Could not find a station by the UUID")
+            sys.exit(0)
+            
+        log.info("Station found: {}".format(self.target_station["name"]))
+        self.API.search(name=self.target_station["name"], name_exact=True)
+        # againg register a valid click
+        if is_ok == "false":
+            res = self.API.click_counter(self.target_station["stationuuid"])
+            log.debug(res)
