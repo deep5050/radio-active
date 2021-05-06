c6f2989eea29ef9c8488d0d97956b393007b3ed6
Dipankar Pal <dipankarpal5050@gmail.com>
2021-05-06 15:55:07 +0530

    feat: now handles alias stations

--- radioactive/last_station.py
+++ radioactive/last_station.py
@@ -20,15 +20,20 @@ class Last_station:
         self.last_station_path = None
 
         self.last_station_path = os.path.join(
-            os.path.expanduser("~"), ".radio-active-last-station.json")
+            os.path.expanduser("~"), ".radio-active-last-station")
 
     def get_info(self):
         try:
             with open(self.last_station_path, "r") as f:
                 last_station = json.load(f)
-                log.info("Playing last station: {}".format(
-                    last_station["name"]))
-                return last_station["stationuuid"]
+                return last_station
+
+                # log.info("Playing last station: {}".format(
+                #     last_station["name"]))
+                # if last_station['alias'] == True:
+                #     # if station was an alias
+                #     return last_station['uuid_or_url']
+                # return last_station["stationuuid"]
         except Exception:
             log.critical(
                 "Need a station name  or UUID to play the radio, see help")
