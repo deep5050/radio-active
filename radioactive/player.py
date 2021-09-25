b2c82c9ef7824297583e4c193cbc371475b60ba3
Restyled.io <commits@restyled.io>
2021-09-25 03:08:52 +0000

    Restyled by yapf

--- radioactive/player.py
+++ radioactive/player.py
@@ -11,11 +11,9 @@ from zenlog import log
 
 
 class Player:
-
     """FFPlayer handler, it holds all the attributes to properly execute ffplay
     FFmepg required to be installed seperately
     """
-
     def __init__(self, URL, volume):
         self.url = URL
         self.volume = volume
@@ -47,7 +45,8 @@ class Player:
             shell=False,
         )
 
-        log.debug("player: ffplay => PID {} initiated".format(self.process.pid))
+        log.debug("player: ffplay => PID {} initiated".format(
+            self.process.pid))
 
         # sleep(3)  # sleeping for 3 seconds wainting for ffplay to start properly
 
