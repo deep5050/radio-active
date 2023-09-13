52351bed9a47ce7962105dd4ae5e0d9861554b25
Dipankar Pal <dipankarpal5050@gmail.com>
2023-09-13 18:37:21 +0530

    added: sentry SDK

--- radioactive/__main__.py
+++ radioactive/__main__.py
@@ -23,15 +23,12 @@ from radioactive.player import Player, kill_background_ffplays
 
 # using sentry to gather unhandled errors at production and will be removed on next major update.
 # I respect your concerns but need this to improve radioactive.
-# import sentry_sdk
+import sentry_sdk
 
-# sentry_sdk.init(
-#     dsn="https://e3c430f3b03f49b6bd9e9d61e7b3dc37@o615507.ingest.sentry.io/5749950",
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
-#     # We recommend adjusting this value in production.
-#     traces_sample_rate=1.0,
-# )
+sentry_sdk.init(
+    dsn="https://e3c430f3b03f49b6bd9e9d61e7b3dc37@o615507.ingest.sentry.io/5749950",
+    traces_sample_rate=1.0,
+)
 
 RED_COLOR = "\033[91m"
 END_COLOR = "\033[0m"
