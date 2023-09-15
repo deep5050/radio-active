cec8f9ed05601db046e2025dab05b1043a9a4522
Dipankar Pal <dipankarpal5050@gmail.com>
2023-09-15 23:48:23 +0530

    refactor: removed unnecessary comments

--- radioactive/__init__.py
+++ radioactive/__init__.py
@@ -1,9 +1,3 @@
 __AUTHOR__ = "Dipankar Pal"
 __EMAIL__ = "dipankarpal5050@gmail.com"
 __WEBSITE__ = "deep5050.github.io"
-
-# def get_version():
-#     return __VERSION__
-
-# if __name__ == "__main__":
-#     get_version()
