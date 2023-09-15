e6275b4ceaa17888a5b00f7f649b3b09cec8ac56
Dipankar Pal <dipankarpal5050@gmail.com>
2023-09-15 23:51:47 +0530

    feat: :zap: app version 2.6.0
    #43 #46 #49

--- radioactive/app.py
+++ radioactive/app.py
@@ -10,7 +10,7 @@ import requests
 
 class App:
     def __init__(self):
-        self.__VERSION__ = "2.5.2"  # change this on every update #
+        self.__VERSION__ = "2.6.0"  # change this on every update #
         self.pypi_api = "https://pypi.org/pypi/radio-active/json"
         self.remote_version = ""
 
